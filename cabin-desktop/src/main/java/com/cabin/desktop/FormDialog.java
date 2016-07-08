dd52e8b463f561897a98fe0638cef949c26c3148
jose <joseruizp@gmail.com>
2016-07-07 19:02:15 -0500

    arreglando issues

--- cabin-desktop/src/main/java/com/cabin/desktop/FormDialog.java
+++ cabin-desktop/src/main/java/com/cabin/desktop/FormDialog.java
@@ -6,6 +6,7 @@ package com.cabin.desktop;
 
 import java.awt.Container;
 import java.awt.Dialog;
+import java.awt.Dimension;
 import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -27,198 +28,176 @@ import com.jgoodies.forms.layout.FormLayout;
  * @author jose ruiz
  */
 public class FormDialog extends JDialog {
-	
-	private static final long serialVersionUID = 1L;
-	private Client client;
-	private Computer computer;
-	
-	public FormDialog(Frame owner) {
-		super(owner);
-		initComponents();
-	}
 
-	public FormDialog(Dialog owner, Client client, Computer computer) {
-		super(owner);
-		System.out.println("in FormDialog");
-		this.client = client;
-		this.computer = computer;
-		initComponents();
-	}
+    private static final long serialVersionUID = 1L;
+    private Client client;
+    private Computer computer;
 
-	private void initComponents() {
-		// JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
-		// Generated using JFormDesigner Evaluation license - jose ruiz
-		userLabel = new JLabel();
-		userValueLabel = new JLabel();
-		label13 = new JLabel();
-		pointsLabel = new JLabel();
-		pointsValueLabel = new JLabel();
-		balanceLabel = new JLabel();
-		balanceValueLabel = new JLabel();
-		experienceLabel = new JLabel();
-		experienceValueLabel = new JLabel();
-		groupLabel = new JLabel();
-		groupValueLabel = new JLabel();
-		tariffLabel = new JLabel();
-		tariffValueLabel = new JLabel();
-		byHourLabel = new JLabel();
-		newPointsLabel = new JLabel();
-		newPointsTextField = new JTextField();
-		bonificationLabel = new JLabel();
-		bonificationValueLabel = new JTextField();
-		viewBonus = new JButton();
-		rentTypeLabel = new JLabel();
-		timeLabel = new JLabel();
-		timeTextField = new JTextField();
-		rentTypeCombobox = new JComboBox();
-		priceLabel = new JLabel();
-		priceTextField = new JTextField();
-		rentButton = new JButton();
-		rentAndExchangeButton = new JButton();
-		cancelButton = new JButton();
+    private JLabel userLabel;
+    private JLabel userValueLabel;
+    private JLabel label13;
+    private JLabel pointsLabel;
+    private JLabel pointsValueLabel;
+    private JLabel balanceLabel;
+    private JLabel balanceValueLabel;
+    private JLabel experienceLabel;
+    private JLabel experienceValueLabel;
+    private JLabel groupLabel;
+    private JLabel groupValueLabel;
+    private JLabel tariffLabel;
+    private JLabel tariffValueLabel;
+    private JLabel byHourLabel;
+    private JLabel newPointsLabel;
+    private JTextField newPointsTextField;
+    private JLabel bonificationLabel;
+    private JLabel bonificationValueLabel;
+    private JButton viewBonus;
+    private JLabel rentTypeLabel;
+    private JComboBox<Object> rentTypeCombobox;
+    private JLabel timeLabel;
+    private JTextField timeTextField;
+    private JLabel priceLabel;
+    private JTextField priceTextField;
+    private JButton rentButton;
+    private JButton rentAndExchangeButton;
+    private JButton cancelButton;
 
-		//======== this ========
-		Container contentPane = getContentPane();
-		contentPane.setLayout(new FormLayout(
-			"30dlu, default, 50dlu, default, 15dlu, 50dlu, 69dlu:grow, default, 40dlu",
-			"default, 9*(19dlu), 20dlu"));
+    public FormDialog(Frame owner) {
+        super(owner);
+        initComponents();
+    }
 
-		//---- label1 ----
-		userLabel.setText("Usuario:");
-		userValueLabel.setText(client.getName());
-		contentPane.add(userLabel, CC.xy(2, 2));
-		contentPane.add(userValueLabel, CC.xywh(3, 2, 4, 1));
-		contentPane.add(label13, CC.xy(1, 3));
+    public FormDialog(Dialog owner, Client client, Computer computer) {
+        super(owner);
+        System.out.println("in FormDialog");
+        this.client = client;
+        this.computer = computer;
+        initComponents();
+    }
 
-		//---- label2 ----
-		pointsLabel.setText("Puntos:");
-		pointsValueLabel.setText(String.valueOf(client.getPoints()));
-		contentPane.add(pointsLabel, CC.xy(2, 3));
-		contentPane.add(pointsValueLabel, CC.xy(3, 3));
+    private void initComponents() {
+        userLabel = new JLabel();
+        userValueLabel = new JLabel();
+        label13 = new JLabel();
+        pointsLabel = new JLabel();
+        pointsValueLabel = new JLabel();
+        balanceLabel = new JLabel();
+        balanceValueLabel = new JLabel();
+        experienceLabel = new JLabel();
+        experienceValueLabel = new JLabel();
+        groupLabel = new JLabel();
+        groupValueLabel = new JLabel();
+        tariffLabel = new JLabel();
+        tariffValueLabel = new JLabel();
+        byHourLabel = new JLabel();
+        newPointsLabel = new JLabel();
+        newPointsTextField = new JTextField();
+        bonificationLabel = new JLabel();
+        bonificationValueLabel = new JLabel();
+        viewBonus = new JButton();
+        rentTypeLabel = new JLabel();
+        timeLabel = new JLabel();
+        timeTextField = new JTextField();
+        rentTypeCombobox = new JComboBox<Object>();
+        priceLabel = new JLabel();
+        priceTextField = new JTextField();
+        rentButton = new JButton();
+        rentAndExchangeButton = new JButton();
+        cancelButton = new JButton();
 
-		//---- label3 ----
-		balanceLabel.setText("Saldo:");
-		balanceValueLabel.setText(String.valueOf(client.getBalance()));
-		contentPane.add(balanceLabel, CC.xy(6, 3));
-		contentPane.add(balanceValueLabel, CC.xy(7, 3));
+        Container contentPane = getContentPane();
+        contentPane.setLayout(new FormLayout("30dlu, default, 50dlu, default, 15dlu, 50dlu, 69dlu:grow, default, 40dlu", "default, 13*(19dlu), 20dlu"));
 
-		//---- label4 ----
-		experienceLabel.setText("Experiencia:");
-		experienceValueLabel.setText(String.valueOf(client.getExperience()));
-		contentPane.add(experienceLabel, CC.xy(8, 3));
-		contentPane.add(experienceValueLabel, CC.xy(9, 3));
+        userLabel.setText("Usuario:");
+        userValueLabel.setText(client.getName());
+        contentPane.add(userLabel, CC.xy(2, 2));
+        contentPane.add(userValueLabel, CC.xywh(3, 2, 4, 1));
+        contentPane.add(label13, CC.xy(1, 3));
 
-		//---- label5 ----
-		groupLabel.setText("Grupo PC:");
-		groupValueLabel.setText(computer.getGroup().getName());
-		contentPane.add(groupLabel, CC.xy(2, 5));
-		contentPane.add(groupValueLabel, CC.xywh(3, 5, 2, 1));
+        pointsLabel.setText("Puntos:");
+        pointsValueLabel.setText(String.valueOf(client.getPoints()));
+        contentPane.add(pointsLabel, CC.xy(2, 3));
+        contentPane.add(pointsValueLabel, CC.xy(3, 3));
 
-		//---- label6 ----
-		tariffLabel.setText("Tarifa:");
-		contentPane.add(tariffLabel, CC.xy(6, 5));
-		contentPane.add(tariffValueLabel, CC.xy(7, 5));
+        balanceLabel.setText("Saldo:");
+        balanceValueLabel.setText(String.valueOf(client.getBalance()));
+        contentPane.add(balanceLabel, CC.xy(6, 3));
+        contentPane.add(balanceValueLabel, CC.xy(7, 3));
 
-		//---- label7 ----
-		byHourLabel.setText("x Hora");
-		contentPane.add(byHourLabel, CC.xy(8, 5));
+        experienceLabel.setText("Experiencia:");
+        experienceValueLabel.setText(String.valueOf(client.getExperience()));
+        contentPane.add(experienceLabel, CC.xy(8, 3));
+        contentPane.add(experienceValueLabel, CC.xy(9, 3));
 
-		//---- label8 ----
-		newPointsLabel.setText("Puntos:");
-		contentPane.add(newPointsLabel, CC.xy(2, 7));
-		contentPane.add(newPointsTextField, CC.xywh(3, 7, 2, 1));
+        groupLabel.setText("Grupo PC:");
+        groupValueLabel.setText(computer.getGroup().getName());
+        contentPane.add(groupLabel, CC.xy(2, 5));
+        contentPane.add(groupValueLabel, CC.xywh(3, 5, 2, 1));
 
-		//---- label9 ----
-		bonificationLabel.setText("Bonificacion:");
-		contentPane.add(bonificationLabel, CC.xy(6, 7));
-		contentPane.add(bonificationValueLabel, CC.xy(7, 7));
-		
-		viewBonus.setText("Ver");
-		contentPane.add(viewBonus, CC.xy(9, 7));
-		viewBonus.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				int points = Integer.parseInt(newPointsTextField.getText());
-				Double balance = new PrizesRuleRest().getBonification(client.getLevel().getId(), points);
-				bonificationValueLabel.setText(String.valueOf(balance));
-			}
-		});
+        tariffLabel.setText("Tarifa:");
+        contentPane.add(tariffLabel, CC.xy(6, 5));
+        contentPane.add(tariffValueLabel, CC.xy(7, 5));
 
-		//---- label11 ----
-		rentTypeLabel.setText("Tipo Alquiler:");
-		contentPane.add(rentTypeLabel, CC.xy(2, 9));
-		contentPane.add(rentTypeCombobox, CC.xy(3, 9));
+        byHourLabel.setText("x Hora");
+        contentPane.add(byHourLabel, CC.xy(8, 5));
 
-		//---- label11 ----
-		timeLabel.setText("Tiempo:");
-		contentPane.add(timeLabel, CC.xy(2, 11));
-		contentPane.add(timeTextField, CC.xy(3, 11));
+        newPointsLabel.setText("Puntos:");
+        contentPane.add(newPointsLabel, CC.xy(2, 7));
+        contentPane.add(newPointsTextField, CC.xywh(3, 7, 2, 1));
 
-		//---- label12 ----
-		priceLabel.setText("Precio:");
-		contentPane.add(priceLabel, CC.xy(6, 11));
-		contentPane.add(priceTextField, CC.xy(7, 11));
+        bonificationLabel.setText("Bonificacion:");
+        contentPane.add(bonificationLabel, CC.xy(6, 7));
+        contentPane.add(bonificationValueLabel, CC.xy(7, 7));
 
-		//---- button1 ----
-		rentButton.setText("Alquilar");
-		rentButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				RentRest rentRest = new RentRest();
-				String rentTime = timeTextField.getText();
-				rentRest.rentComputer(client.getId(), computer.getId(), rentTime);
-			}
-		});
-		contentPane.add(rentButton, CC.xy(4, 13));
-		
-		rentAndExchangeButton.setText("Alquilar y Canjear");
-		rentAndExchangeButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				RentRest rentRest = new RentRest();
-				String rentTime = timeTextField.getText();
-				rentRest.rentComputer(client.getId(), computer.getId(), rentTime);
-			}
-		});
-		contentPane.add(rentAndExchangeButton, CC.xy(6, 13));
+        viewBonus.setText("Ver");
+        contentPane.add(viewBonus, CC.xy(8, 7));
+        viewBonus.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("entra viewBonus");
+                int points = Integer.parseInt(newPointsTextField.getText());
+                System.out.println("entra viewBonus, points: " + points);
+                Double balance = new PrizesRuleRest().getBonification(client.getLevel().getId(), points);
+                System.out.println("entra viewBonus, balance: " + balance);
+                bonificationValueLabel.setText(String.valueOf(balance));
+            }
+        });
 
-		//---- button2 ----
-		cancelButton.setText("Cancelar");
-		contentPane.add(cancelButton, CC.xy(8, 13));
-		pack();
-		setLocationRelativeTo(getOwner());
-		// JFormDesigner - End of component initialization  //GEN-END:initComponents
-		
-		setVisible(true);
-	}
+        rentTypeLabel.setText("Tipo Alquiler:");
+        contentPane.add(rentTypeLabel, CC.xy(2, 9));
+        contentPane.add(rentTypeCombobox, CC.xy(3, 9));
 
-	// JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
-	// Generated using JFormDesigner Evaluation license - jose ruiz
-	private JLabel userLabel;
-	private JLabel userValueLabel;
-	private JLabel label13;
-	private JLabel pointsLabel;
-	private JLabel pointsValueLabel;
-	private JLabel balanceLabel;
-	private JLabel balanceValueLabel;
-	private JLabel experienceLabel;
-	private JLabel experienceValueLabel;
-	private JLabel groupLabel;
-	private JLabel groupValueLabel;
-	private JLabel tariffLabel;
-	private JLabel tariffValueLabel;
-	private JLabel byHourLabel;
-	private JLabel newPointsLabel;
-	private JTextField newPointsTextField;
-	private JLabel bonificationLabel;
-	private JTextField bonificationValueLabel;
-	private JButton viewBonus;
-	private JLabel rentTypeLabel;
-	private JComboBox rentTypeCombobox;
-	private JLabel timeLabel;
-	private JTextField timeTextField;
-	private JLabel priceLabel;
-	private JTextField priceTextField;
-	private JButton rentButton;
-	private JButton rentAndExchangeButton;
-	private JButton cancelButton;
-	// JFormDesigner - End of variables declaration  //GEN-END:variables
+        timeLabel.setText("Tiempo:");
+        contentPane.add(timeLabel, CC.xy(2, 11));
+        contentPane.add(timeTextField, CC.xy(3, 11));
+
+        priceLabel.setText("Precio:");
+        contentPane.add(priceLabel, CC.xy(6, 11));
+        contentPane.add(priceTextField, CC.xy(7, 11));
+
+        rentButton.setText("Alquilar");
+        rentButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                RentRest rentRest = new RentRest();
+                String rentTime = timeTextField.getText();
+                rentRest.rentComputer(client.getId(), computer.getId(), rentTime);
+            }
+        });
+        contentPane.add(rentButton, CC.xy(4, 13));
+
+        rentAndExchangeButton.setText("Alquilar y Canjear");
+        rentAndExchangeButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                RentRest rentRest = new RentRest();
+                String rentTime = timeTextField.getText();
+                rentRest.rentComputer(client.getId(), computer.getId(), rentTime);
+            }
+        });
+        contentPane.add(rentAndExchangeButton, CC.xyw(6, 13, 2));
+
+        pack();
+        setLocationRelativeTo(getOwner());
+
+        setVisible(true);
+    }
 
 }
